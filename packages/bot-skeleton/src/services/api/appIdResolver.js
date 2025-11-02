6ac05b23a958b57e7019431d5cdc12978ff5c2c0
Brian <ndanumumo93@gmail.com>
2025-11-02 15:53:06 +0300

    mumo

---
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '110113', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '110113',
+    'app.deriv.com': '110113',
+    'staging-app.deriv.com': '110113',
+    'goon-puce.vercel.app': '110113',
+});
+
+export default AppIds;
